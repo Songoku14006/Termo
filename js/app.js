60d8c48da35a718c839ce13e40605a8cb1828a2e
IsacGama <diasgamaisac0@gmail.com>
2024-06-30 17:12:12 -0300

    Em andamento

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 import { palavras } from './palavras.js';
 
+let size = palavras.length;
 
+function getRandomNumber(size) {
+    let randomNumber = Math.random() * size-1;
+    return randomNumber;
+}
+document.addEventListener('DOMContentLoaded', getRandomWord());
+
+function getRandomWord() {
+    let posicao = getRandomNumber(size);
+    let fruit = palavras.at(posicao);
+    console.log(fruit);
+}
