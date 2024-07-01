b31a061c1cb5e112a657a9594b67bb92ca1f2c9f
Songoku14006 <diasgamaisac0@gmail.com>
2024-07-01 18:24:56 -0300

    a

--- js/app.js
+++ js/app.js
@@ -1,15 +1,54 @@
 import { palavras } from './palavras.js';
 
-let size = palavras.length;
+document.addEventListener("DOMContentLoaded", function() {
+    let currentRow = 1;
+    let size = palavras.length;
+    let selectedIndex = 1;    
+    let selectInput = document.getElementById(`main-input-${selectedIndex}`);
+    selectInput.focus();   
 
-function getRandomNumber(size) {
-    let randomNumber = Math.random() * size-1;
-    return randomNumber;
-}
-document.addEventListener('DOMContentLoaded', getRandomWord());
+    function insertLetter(event) {
+        const letter = event.target.textContent;
+        const rowInputs = document.querySelectorAll(`#row-${currentRow} .main-input`);
+        
+        for (let i = 0; i < rowInputs.length; i++) {
+            if (rowInputs[i].value === "") {
+                rowInputs[i].value = letter;
+                rowInputs[i].focus();
+                break;
+            }
+        }
+    }
+    
+    const letras = document.querySelectorAll(".footer-teclado");
+    letras.forEach(function(letra) {
+        letra.addEventListener("click", insertLetter);
+    });
 
-function getRandomWord() {
-    let posicao = getRandomNumber(size);
-    let fruit = palavras.at(posicao);
-    console.log(fruit);
-}
+    document.addEventListener('DOMContentLoaded', getRandomWord());
+    function getRandomWord() {
+        let posicao = getRandomNumber(size);
+        let fruit = palavras.at(posicao);
+        console.log(fruit);
+    }
+
+    function getRandomNumber(size) {
+        let randomNumber = Math.random() * size-1;
+        return randomNumber;
+    }
+    const backspace = document.querySelector(".div-backspace-button");
+    if (backspace) {
+        backspace.addEventListener("click", btnBackspace);
+    }
+
+    function btnBackspace() {
+        const rowInputs = document.querySelectorAll(`#row-${currentRow} .main-input`);
+        for (let i = rowInputs.length - 1; i >= 0; i--) {
+            if (rowInputs[i].value !== "") {
+                rowInputs[i].value = "";
+                rowInputs[selectedIndex].focus;
+                break;
+            }
+        }
+    }
+});
